0e87709caf4129e1266d9b104174920a3d6cbe4c
Mark E. Carter <markeverettcarter@gmail.com>
2021-04-13 18:07:33 -0700

    Add ERC verbiage to the footer

--- components/SiteFooter/SiteFooter.php
+++ components/SiteFooter/SiteFooter.php
@@ -7,6 +7,7 @@
       <li>
         <a href="mailto:info@globalfuturehealth.org">info@globalfuturehealth.org</a>
       </li>
+      <li>This project has received funding from the European Research Council (ERC) under the European Union’s Horizon 2020 research and innovation programme (Grant agreement No. 759414)</li>
     </ul>
     <ul id="SiteFooter-Sponsors">
       <li id="logo-aissr">AISSR</li>
